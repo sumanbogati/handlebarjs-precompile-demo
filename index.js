455eb8d8e11bf0a6398ced2a785ce00ee9468777
suman <sumanbogati@gmail.com>
2018-04-11 16:40:31 +0530

    Formatting code
    Formatting code

--- index.js
+++ index.js
@@ -13,23 +13,19 @@ window.onload = function (){
 	var html = template(context);
 	$(document.body).append(html);
 
-    var context2 = { "quiz" : "physics"};
-    var template2 = JST['templates/quiz.hbs'];
-    var html2 = template2(context2);
-    $(document.body).append(html2);
+	var context2 = { "quiz" : "physics"};
+	var template2 = JST['templates/quiz.hbs'];
+	var html2 = template2(context2);
+	$(document.body).append(html2);
 
+	var context3 = { "home" : "main home"};
+	var template3 = JST['templates/home.hbs'];
+	var html3 = template3(context3);
+	$(document.body).append(html3);
 
-    var context3 = { "home" : "main home"};
-    var template3 = JST['templates/home.hbs'];
-    var html3 = template3(context3);
-    $(document.body).append(html3);
-
-
-
-    var context4 = { "report" : "student poll"};
-    var template4 = JST['templates/report.hbs'];
-    var html4 = template4(context4);
-    $(document.body).append(html4);
-
+	var context4 = { "report" : "student poll"};
+	var template4 = JST['templates/report.hbs'];
+	var html4 = template4(context4);
+	$(document.body).append(html4);
 }
 
